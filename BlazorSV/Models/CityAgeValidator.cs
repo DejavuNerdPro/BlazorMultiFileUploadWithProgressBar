70d39fe842487f713b6a560fe08d63de79ea1d37
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2023-12-24 23:26:04 +0630

    Add project files.

---
+++ BlazorSV/Models/CityAgeValidator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSV.Models
+{
+    public class CityAgeValidator : ComponentBase
+    {
+        [Parameter]
+        public string City { get; set; }
+
+        [Parameter]
+        public int Age { get; set; }
+
+        [CascadingParameter]
+        public EditContext EC { get; set; }
+
+        protected override void OnInitialized()
+        {
+            ValidationMessageStore store = new ValidationMessageStore(EC);
+            EC.OnFieldChanged += (sender, args) =>
+            {
+                string name = args.FieldIdentifier.FieldName;
+                if (name == "City" || name == "Age")
+                {
+                    Validate(EC.Model as Employee, store);
+                }
+            };
+        }
+
+        private void Validate(Employee model, ValidationMessageStore store)
+        {
+            if (model.City == City && (model.Age == Age))
+            {
+                string message = City + " does not allow " + Age + " years person for job";
+                store.Add(EC.Field("Age"), message);
+            }
+            else
+                store.Clear();
+            EC.NotifyValidationStateChanged();
+        }
+    }
+}
---
+++ BlazorSV/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSV.Models
+{
+	public class Employee
+	{
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string City { get; set; }
+
+        [Range(30, 32)]
+        public int Age { get; set; }
+    }
+}
